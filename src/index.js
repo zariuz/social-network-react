d5dce484be894c3c9998d20ec85d2eb399fb694c
zariuz <nechetov@hotmail.com>
2020-02-23 22:50:52 +0300

    change index and state, del render

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { addPost, updateNewPostText, subscribe } from './redux/state';
+import './index.css';
+import App from './App';
 import state from './redux/state';
 import * as serviceWorker from './serviceWorker';
-import { rerenderEntireTree } from './render';
+
+let rerenderEntireTree = state => {
+  ReactDOM.render(
+    <App
+      state={state}
+      addPost={addPost}
+      updateNewPostText={updateNewPostText}
+    />,
+    document.getElementById('root')
+  );
+};
 
 rerenderEntireTree(state);
+
+subscribe(rerenderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,6 @@
-import { rerenderEntireTree } from './../render';
+let rerenderEntireTree = () => {
+  console.log('State is changed');
+};
 
 let state = {
   profilePage: {
@@ -28,7 +30,7 @@ let state = {
   sidebar: {}
 };
 
-export let addPost = () => {
+export const addPost = () => {
   let newPost = {
     id: 5,
     message: state.profilePage.newPostText,
@@ -39,10 +41,14 @@ export let addPost = () => {
   rerenderEntireTree(state);
 };
 
-export let updateNewPostText = newText => {
+export const updateNewPostText = newText => {
   state.profilePage.newPostText = newText;
 
   rerenderEntireTree(state);
+};
+
+export const subscribe = observer => {
+  rerenderEntireTree = observer;
 };
 
 export default state;
