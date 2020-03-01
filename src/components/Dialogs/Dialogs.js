f887ee12eba748e61f63a9d47b6cabdbbd7060c7
zariuz <nechetov@hotmail.com>
2020-03-01 23:09:40 +0300

    reducer

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -5,7 +5,7 @@ import Message from './Message/Message';
 import {
   updateNewMessageBodyCreator,
   sendMessageCreator
-} from './../../redux/state';
+} from './../../redux/dialogs-reducer';
 
 const Dialogs = props => {
   let state = props.store.getState().dialogsPage;
