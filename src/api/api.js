acba221add3cd7a9dce4a182da9a16578e9dfee1
zariuz <nechetov@hotmail.com>
2020-03-24 23:26:51 +0300

    usersAPI

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,17 @@
 import * as axios from 'axios';
 
-export const getUsers = (currentPage = 2, pageSize = 10) => {
-  return axios
-    .get(
-      `https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`,
-      {
-        withCredentials: true
-      }
-    )
-    .then(response => response.data);
+const instance = axios.create({
+  withCredentials: true,
+  baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+  headers: {
+    'API-KEY': 'a581eddc-6b2a-4437-9b2a-cfc6d6cb8bb3'
+  }
+});
+
+export const usersAPI = {
+  getUsers(currentPage = 2, pageSize = 10) {
+    return instance
+      .get(`users?page=${currentPage}&count=${pageSize}`)
+      .then(response => response.data);
+  }
 };
