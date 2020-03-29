8f2058c336be532feaa370e72dd9de044b309ec2
zariuz <nechetov@hotmail.com>
2020-03-30 00:09:56 +0300

    test count

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import s from './Settings.module.css';
+import React, { Component } from 'react';
 
-const Settings = () => {
-  return <div>I'am Settings</div>;
-};
+class Settings extends Component {
+  state = { count: 0 };
+
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.increment}>Increment</button>
+      </div>
+    );
+  }
+}
 
 export default Settings;
