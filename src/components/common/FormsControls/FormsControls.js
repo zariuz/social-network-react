26f50fc9cb53f792ce53983c16e886d18db1302b
zariuz <nechetov@hotmail.com>
2020-04-21 12:07:18 +0300

    login form edit

--- src/components/common/FormsControls/FormsControls.js
+++ src/components/common/FormsControls/FormsControls.js
@@ -1,24 +1,34 @@
 import React from 'react';
 import styles from './FormsControls.module.css';
 
-export const Textarea = ({ input, meta, type, placeholder }) => {
-  const hasError = meta.touched && meta.error;
+export const Textarea = ({
+  input,
+  meta: { touched, error },
+  type,
+  placeholder,
+}) => {
+  const hasError = touched && error;
 
   return (
     <div className={`${styles.formControl} ${hasError ? styles.error : ''}`}>
       <textarea {...input} type={type} placeholder={placeholder} />
-      {hasError && <span>{meta.error}</span>}
+      {hasError && <span>{error}</span>}
     </div>
   );
 };
 
-export const Input = ({ input, meta, type, placeholder }) => {
-  const hasError = meta.touched && meta.error;
+export const Input = ({
+  input,
+  meta: { touched, error },
+  type,
+  placeholder,
+}) => {
+  const hasError = touched && error;
 
   return (
     <div className={`${styles.formControl} ${hasError ? styles.error : ''}`}>
       <input {...input} type={type} placeholder={placeholder} />
-      {hasError && <span>{meta.error}</span>}
+      {hasError && <span>{error}</span>}
     </div>
   );
 };
